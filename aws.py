4a4adc42978622e4a565cf6d71172de863c73114
Brodie Blackburn <brodie.blackburn@gmail.com>
2023-07-06 19:25:02 +1000

    Specify intermediary thumbprints of both GitHub Actions certificates

--- aws.py
+++ aws.py
@@ -86,6 +86,11 @@ class Account(cdk.Stack):
             id="GithubProvider",
             url="https://token.actions.githubusercontent.com",
             client_ids=["sts.amazonaws.com"],
+            # See https://github.blog/changelog/2023-06-27-github-actions-update-on-oidc-integration-with-aws/
+            thumbprints=[
+                "6938fd4d98bab03faadb97b34396831e3780aea1",
+                "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
+            ],
         )
 
         # Configure a role assumed by the GitHub identity provider.
